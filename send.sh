1c1fda0817f2b014aef19bf59a31f0f953a3f53c
Matheus Faustino <matheuspfaustino@gmail.com>
2017-10-19 21:16:36 -0200

    Create shell script to send app to 3ds (3dslink)

---
+++ send.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# exit error
+set -e
+
+# compile
+eval "make"
+
+set +e
+
+APP="reddit_3dshack"
+IP="192.168.1.131"
+RETRY=2
+CMD="$DEVKITARM/bin/3dslink --address=$IP $APP.3dsx"
+
+eval $CMD
+
+while [[ $? -ne 0 ]]; do
+    eval $CMD
+done
